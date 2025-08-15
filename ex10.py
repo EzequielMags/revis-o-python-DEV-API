501e908d84d245c1385fac9c87312bb3cde38888
Netto <lucasbtosta@gamil.com>
2025-08-15 11:09:39 -0300

    alterando o ex 10

--- ex10.py
+++ ex10.py
@@ -1,12 +1,5 @@
 import requests 
 
-zonas_sp = {
-    "norte": ["Santana", "Tucuruvi", "Jaçanã", "Tremembé", "Casa Verde", "Limão", "Freguesia do Ó", "Brasilândia"],
-    "sul": ["Campo Limpo", "Capão Redondo", "Jardim Ângela", "Jardim São Luís", "Grajaú", "Parelheiros", "Marsilac"],
-    "leste": ["Itaquera", "Penha", "São Mateus", "Guaianases", "Cidade Tiradentes", "Mooca", "Brás"],
-    "oeste": ["Pinheiros", "Butantã", "Lapa", "Perdizes", "Vila Leopoldina", "Vila Madalena", "Barra Funda"],
-    "centro": ["Sé", "Liberdade", "República", "Santa Cecília", "Consolação", "Bela Vista", "Vila Buarque"]
-}
 
 
 cep = input("Digite o CEP: ")
@@ -18,7 +11,14 @@ print(f"Bairro: {dados['bairro']}, {dados['localidade']} - {dados['uf']}")
 if dados["localidade"] != "São Paulo":
     print("CEP não é de São Paulo.")
 
-for zona in zonas_sp:
-    if dados["bairro"] in zonas_sp[zona]:
-        print(f"O bairro {dados['bairro']} pertence à zona {zona}.")
-        break
+
+
+area = cep[:5]
+if area < "1600":
+    print("Região: Sul")
+elif area < "3000":
+    print("Região: Norte")  
+elif area < "6000":
+    print("Região: Leste")
+elif area < "8000":
+    print("Região: Oeste")
